74bb370f7bbf8ba5e139d50c0f6d3cd8c8a21748
asfaagsgfvfgs <gouravkumarmeena2009123@outlook.com>
2026-05-14 12:03:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,13 @@
 
+#!/bin/bash
+
+echo "Starting container setup..."
+echo "Operating System Info:"
+cat /etc/os-release
+
+echo ""
+echo "Running a bash command: Listing contents of the current directory (/app):"
+ls -la
+
+echo ""
+echo "Container task completed successfully!"
